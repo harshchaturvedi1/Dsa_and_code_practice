083de481ff31f9ae3b1a1c85c2ff932ae9600f0f
Harsh Chaturvedi <harshchaturvedi589@gmail.com>
2022-04-13 12:58:32 +0530

    pocker logics done

--- React/pocker-game/src/Components/Main.jsx
+++ React/pocker-game/src/Components/Main.jsx
@@ -1,6 +1,18 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { RoyalFlush } from "../assets/assets";
+import {
+  totalValue,
+  royalFlush,
+  straightFlush,
+  fourOfKind,
+  fullHouse,
+  flush,
+  straight,
+  threeOfKind,
+  twoPair,
+  pair,
+  highCard,
+} from "../assets/assets";
 import { DisplayCards } from "./DisplayCards";
 import "./Main.css";
 const types = ["hearts", "clubs", "spades", "diamonds"];
@@ -14,12 +26,20 @@ for (let i = 0; i < 4; i++) {
   }
 }
 
+let temp = [
+  { type: "a", value: 1 },
+  { type: "a", value: 1 },
+  { type: "a", value: 1 },
+  { type: "a", value: 2 },
+  { type: "b", value: 2 },
+];
+
 export const Main = () => {
   const [player1, setPlayer1] = useState([]);
   const [player2, setPlayer2] = useState([]);
 
-  const [name1, setName1] = useState("");
-  const [name2, setName2] = useState("");
+  const [name1, setName1] = useState("h");
+  const [name2, setName2] = useState("a");
 
   const acceptNames = () => {
     let temp = "";
@@ -58,18 +78,76 @@ export const Main = () => {
     }
   };
 
+  const tieBreaker = () => {
+    if (totalValue(player1) > totalValue(player2)) {
+      console.log("player 1");
+    } else if (totalValue(player2) > totalValue(player1)) {
+      console.log("player 2");
+    } else {
+      console.log("tie");
+    }
+  };
+
   const calcaulateWinner = () => {
-    console.log(RoyalFlush, typeof RoyalFlush);
-    RoyalFlush(player1);
+    // console.log(player1);
+
+    //check if player 1 is royal flush
+    if (royalFlush(player1)) {
+      // if player 2 is also royal flush
+      if (royalFlush(player2)) {
+        console.log(tieBreaker());
+      }
+      // else if player 2 is not royal flush
+      else {
+        console.log("player 1");
+      }
+    }
+    // else if player 2 is royal flush
+    else if (royalFlush(player2)) {
+      console.log("player 2");
+    }
+    //check if player 1 is straight flush
+    else if (straightFlush(player1)) {
+      // check if player 2 is also straight flush
+      if (straightFlush(player2)) {
+        tieBreaker();
+      }
+      // if player 2 is  not a straight flush
+      else {
+        console.log("player 1");
+      }
+    }
+    // if only player 2 is straight flush
+    else if (straightFlush(player2)) {
+      console.log("player 2");
+    }
+    // if player 1 is four of a kind
+    else if (fourOfKind(player1)) {
+      // if player 2 is also four of a kind
+      if (fourOfKind(player2)) {
+        tieBreaker();
+      }
+      // if only player 1 is four of a kind
+      else {
+        console.log("player 1");
+      }
+    }
+    // if only player 2 is four of a kind
+    else if (fourOfKind(player2)) {
+      console.log("player 2");
+    }
   };
 
   useEffect(() => {
-    acceptNames();
+    // acceptNames();
     fillValues();
   }, []);
 
   return (
     <>
+      <button onClick={calcaulateWinner} className="winner-button">
+        FindWinner
+      </button>
       <div className="main">
         <div className="firstDiv">
           <h3>{name1}</h3>
@@ -80,9 +158,6 @@ export const Main = () => {
           <DisplayCards player={player2} />
         </div>
       </div>
-      <button onClick={calcaulateWinner} className="winner-button">
-        FindWinner
-      </button>
     </>
   );
 };
--- React/pocker-game/src/assets/assets.js
+++ React/pocker-game/src/assets/assets.js
@@ -1,6 +1,182 @@
-function RoyalFlush(cards) {
-  cards = cards.sort((a, b) => a.value - b.value);
-  console.log(cards);
+function totalValue(cards) {
+  let total = 0;
+  cards.forEach((e) => (total += e.value));
+  return total;
 }
 
-export { RoyalFlush };
+function royalFlush(cards) {
+  // five cards in sequence , all of royal category and of same suit
+  cards = cards.sort((a, b) => a.value - b.value);
+  for (let i = 1; i < 5; i++) {
+    if (cards[i].type !== cards[i - 1].type) return false;
+  }
+  if (cards[4].value === 14 && cards[0].value === 10) return true;
+  else return false;
+}
+
+function straightFlush(cards) {
+  // five cards in sequence of same suit
+  cards = cards.sort((a, b) => a.value - b.value);
+  for (let i = 1; i < 5; i++) {
+    if (
+      cards[i].value !== cards[i - 1].value + 1 ||
+      cards[i].type !== cards[i - 1].type
+    )
+      return false;
+  }
+  return true;
+}
+
+function fourOfKind(cards) {
+  // four cards of one rank(any category) and one any other unmatched card
+  let obj = {};
+  cards.forEach((e) => {
+    if (!obj[e.value]) {
+      obj[e.value] = 1;
+    } else {
+      let p = obj[e.value];
+      obj[e.value] = p + 1;
+    }
+  });
+  let keys = Object.keys(obj);
+
+  if (keys.length !== 2) {
+    return false;
+  } else if (obj[keys[0]] === 1 || obj[keys[0]] === 4) {
+    return true;
+  } else return false;
+}
+
+function fullHouse(cards) {
+  // three matching cards of a rank and 2 matching cards of one rank
+  let obj = {};
+  cards.forEach((e) => {
+    if (!obj[e.value]) {
+      obj[e.value] = 1;
+    } else {
+      let p = obj[e.value];
+      obj[e.value] = p + 1;
+    }
+  });
+
+  let keys = Object.keys(obj);
+  if (keys.length !== 2) {
+    return false;
+  } else if (obj[keys[0]] === 3 || obj[keys[0]] === 2) {
+    return true;
+  } else return false;
+}
+
+function flush(cards) {
+  // all five cards of same suit but not insequence
+  for (let i = 1; i < 5; i++) {
+    if (cards[i].type !== cards[i - 1].type) return false;
+  }
+  return true;
+}
+
+function straight(cards) {
+  // cards in sequential rank and atleast two different suits
+  cards = cards.sort((a, b) => a.value - b.value);
+  for (let i = 1; i < 5; i++) {
+    if (cards[i].value !== cards[i - 1].value + 1) return false;
+  }
+  let obj = {};
+  cards.forEach((e) => {
+    if (!obj[e.type]) {
+      obj[e.type] = 1;
+    } else {
+      let p = obj[e.type];
+      obj[e.type] = p + 1;
+    }
+  });
+
+  let keys = Object.keys(obj);
+  if (keys.length < 2) return false;
+  return true;
+}
+
+function threeOfKind(cards) {
+  // three cards of same rank(not necessary of same suit) and two other cards not of this rank nor same as each other
+  let obj = {};
+  cards.forEach((e) => {
+    if (!obj[e.value]) {
+      obj[e.value] = 1;
+    } else {
+      let p = obj[e.value];
+      obj[e.value] = p + 1;
+    }
+  });
+
+  let keys = Object.keys(obj);
+  if (keys.length !== 3) return false;
+  for (let i = 0; i < keys.length; i++) {
+    if (obj[keys[i]] !== 3 || obj[keys[i]] !== 1) return false;
+  }
+  return true;
+}
+
+function twoPair(cards) {
+  // two pairs of two cards of same rank and one other card
+  let obj = {};
+  cards.forEach((e) => {
+    if (!obj[e.value]) {
+      obj[e.value] = 1;
+    } else {
+      let p = obj[e.value];
+      obj[e.value] = p + 1;
+    }
+  });
+
+  let keys = Object.keys(obj);
+  if (keys.length !== 3) return false;
+  for (let i = 0; i < keys.length; i++) {
+    if (obj[keys[i]] !== 2 || obj[keys[i]] !== 1) return false;
+  }
+  return true;
+}
+
+function pair(cards) {
+  // two cards of one rank , plus three cards of other ranks and differnt from each other
+  let obj = {};
+  cards.forEach((e) => {
+    if (!obj[e.value]) {
+      obj[e.value] = 1;
+    } else {
+      let p = obj[e.value];
+      obj[e.value] = p + 1;
+    }
+  });
+
+  let keys = Object.keys(obj);
+  if (keys.length !== 4) return false;
+  for (let i = 0; i < keys.length; i++) {
+    if (obj[keys[i]] !== 2 || obj[keys[i]] !== 1) return false;
+  }
+  return true;
+}
+
+function highCard(player1, player2) {
+  player1.sort((a, b) => a.value - b.value);
+  player2.sort((a, b) => a.value - b.value);
+  for (let i = 0; i < 5; i++) {
+    if (player1.value > player2.value) {
+      return "player1";
+    } else if (player1.value < player2.value) {
+      return "player2";
+    }
+  }
+}
+export {
+  royalFlush,
+  totalValue,
+  straightFlush,
+  fourOfKind,
+  fullHouse,
+  flush,
+  straight,
+  threeOfKind,
+  twoPair,
+  pair,
+  highCard,
+};
